cc41a827d29e27e5cd6fa2ac9da4b92e9719e54a
manika <manikamanipathak@gmail.com>
2024-09-27 16:42:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// Import the Express module
+const express = require('express');
+
+// Create an instance of an Express app
+const app = express();
+
+// Define a port number
+const PORT = 3000;
+
+// Define a simple API route to respond with "Connection Established"
+app.get('/api/connection', (req, res) => {
+    res.send('Connection Established');
+});
+
+// Start the server and listen on the defined port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
